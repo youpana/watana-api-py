40bf4802f68f8e982f9d04d3f43b910fbec9dc1e
Jose Acacio <jose@youpana.com>
2023-05-16 18:38:08 -0500

    README.md

--- examples/consultar_carpeta.py
+++ examples/consultar_carpeta.py
@@ -5,8 +5,8 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from WatanaApi.watana import *
 
-ruta = 'http://localhost:6001/api/v1/proveedor/lyLny3VbSmrlw8mJ9a1Io9n1hMAsbUvKBlG9CZrvu68'
-token = 'eyJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6ImYwYjhkOTJkMTA1YTFjM2JhYTM0MjE3MDg2YTY4NDg1N2ExM2JjMzFkODJhOWU0ZCJ9.a_9AQUWLRk-ga4jhRVt_4adMHKFdP5lqVSxpquMmqlE'
+ruta = '<RUTA>'
+token = '<TOKEN>'
 # Crear una instancia de WatanaApi
 api = WatanaApi(ruta, token)
 
